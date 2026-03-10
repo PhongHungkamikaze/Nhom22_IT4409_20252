97fb54ef031cb2efe64a4fe6a5d686786e21a1ed
PhongHungkamikaze <phongpcbyl@gmail.com>
2026-03-10 15:39:52 +0700

    add CRUD Question

--- Backend/exam_online/exam/admin.py
+++ Backend/exam_online/exam/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Quiz
+from .models import Quiz, User, Question
 # Register your models here.
 @admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
     pass
--- Backend/exam_online/exam/serializer.py
+++ Backend/exam_online/exam/serializer.py
@@ -1,21 +1,30 @@
 from rest_framework import serializers
+from rest_framework.decorators import action
 from .models import Question, Quiz
 
-class QuestionSerializer(serializers.Serializer):
-    model = Question
+
+class QuestionSerializer(serializers.ModelSerializer):
+    quiz_title = serializers.CharField(source="quiz.title", read_only=True)
+
     class Meta:
-        fields = '__all__'
+        model = Question
+        fields = ["quiz", "type", "content", "quiz_title",]
+
+    @action(detail=True, methods=["get"])
+    def question(self, request, pk=None):
+        pass
+
 
 class QuizSerializer(serializers.ModelSerializer):
-    author_name = serializers.CharField(source="author.username", read_only = True)
+    author_name = serializers.CharField(source="author.username", read_only=True)
+
     class Meta:
         model = Quiz
-        fields = ["title",
-                  "description",
-                  "author",
-                  "time_limit",
-                  "author_name",
-                ]
-        extra_kwargs = {
-            "created_at" : {"read_only": True}
-        }
+        fields = [
+            "title",
+            "description",
+            "author",
+            "time_limit",
+            "author_name",
+        ]
+        extra_kwargs = {"created_at": {"read_only": True}}

895be170b46a1f9c7077c1d1c77fce8fa83e7608
Phong2511 <123247133+Phong2511@users.noreply.github.com>
2026-05-20 21:40:22 +0700

    Rename Quizzes export to MyQuizzes

--- Frontend/src/pages/Admin/index.js
+++ Frontend/src/pages/Admin/index.js
@@ -1,6 +1,7 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Users } from './Users';
-export { default as Quizzes } from './Quizzes';
+export { default as Quizzes } from './MyQuizzes';
 export { default as Questions } from './Questions';
 export { default as Attempts } from './Attempts';
 export { default as Analytics } from './Analytics';
+export { default as Subjects } from './Subjects';
